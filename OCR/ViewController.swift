06dfd46999bd7299f3c9dad1070e8afce5b9ab75
Александр Игнатьев <izh.sever@gmail.com>
2015-03-17 20:33:22 +0300

    create project

---
+++ OCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Александр Игнатьев on 17.03.15.
+//  Copyright (c) 2015 Alexander Ignition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
