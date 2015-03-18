12bbf5e07feb44381f66a6bf86d48eceb6b7b8eb
GRADUATION <izh.sever@gmail.com>
2015-03-18 16:46:49 +0300

    Tesseract extension

--- OCR/ViewController.swift
+++ OCR/ViewController.swift
@@ -12,10 +12,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var tesseract = Tesseract();
     }
     
-    // MARK: - Actions
+    // MARK: Actions
 
     @IBAction func cameraAction(sender: UIBarButtonItem) {
         let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Photo", message: nil, preferredStyle: .ActionSheet)
@@ -33,33 +32,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         imagePickerActionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel) { (alert) -> Void in })
         presentViewController(imagePickerActionSheet, animated: true, completion: nil)
     }
+
     
-    // MARK: - Scale
-    
-    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
-        
-        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
-        var scaleFactor: CGFloat
-        
-        if image.size.width > image.size.height {
-            scaleFactor = image.size.height / image.size.width
-            scaledSize.width = maxDimension
-            scaledSize.height = scaledSize.width * scaleFactor
-        } else {
-            scaleFactor = image.size.width / image.size.height
-            scaledSize.height = maxDimension
-            scaledSize.width = scaledSize.height * scaleFactor
-        }
-        
-        UIGraphicsBeginImageContext(scaledSize)
-        image.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
-        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return scaledImage
-    }
-    
-    // MARK: - UIImagePickerController
+}
+
+// MARK: - UIImagePickerController
+
+extension ViewController: UIImagePickerControllerDelegate {
     
     func showImagePickerControllerWithType(type: UIImagePickerControllerSourceType) {
         let picker = imagePickerControllerWithType(type)
@@ -72,12 +51,79 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         picker.sourceType = type
         return picker;
     }
+
+    // MARK: UIImagePickerControllerDelegate
     
-
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        let selectedPhoto = info[UIImagePickerControllerOriginalImage] as UIImage
+        let scaledImage = selectedPhoto.scaleImage(maxDimension: 640)
+        
+        dismissViewControllerAnimated(true) {
+            self.performImageRecognition(scaledImage)
+        }
+    }
+    
+    
 }
 
-extension ViewController: UIImagePickerControllerDelegate {
-    //
+// MARK: - Tesseract
+
+extension ViewController: TesseractDelegate {
+    
+    func performImageRecognition(image: UIImage) {
+        let tesseract = Tesseract()
+        tesseract.language = "eng+fra"
+        tesseract.recognize()
+    }
+    
+    // MARK: TesseractDelegate
+    
+    func progressImageRecognitionForTesseract(tesseract: Tesseract!) {
+        //
+    }
+    
+    func shouldCancelImageRecognitionForTesseract(tesseract: Tesseract!) -> Bool {
+        return false;
+    }
+    
+    
 }
 
+// MARK: - Scale Image
+
+extension UIImage {
+    
+    func scaleImage(#maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        let width = self.size.width;
+        let height = self.size.height
+        
+        if width > height {
+            scaleFactor = height / width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = width / height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        return scaleImage(size: scaledSize)
+    }
+    
+    func scaleImage(size scaledSize: CGSize) -> UIImage {
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        self.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+    
+    
+}
 
