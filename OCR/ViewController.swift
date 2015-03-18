ba6939736cc7e2eb02f2a6cec42f3f1edeae3849
GRADUATION <izh.sever@gmail.com>
2015-03-18 15:58:44 +0300

    UIImagePickerController

--- OCR/ViewController.swift
+++ OCR/ViewController.swift
@@ -8,18 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         var tesseract = Tesseract();
     }
+    
+    // MARK: - Actions
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraAction(sender: UIBarButtonItem) {
+        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Photo", message: nil, preferredStyle: .ActionSheet)
+        
+        func alertAction(#title: String, withImagePickerController type: UIImagePickerControllerSourceType) -> UIAlertAction {
+            return UIAlertAction(title: title, style: .Default) { (alert) -> Void in
+                self.showImagePickerControllerWithType(type)
+            }
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            imagePickerActionSheet.addAction(alertAction(title: "Take Photo", withImagePickerController: .Camera))
+        }
+        imagePickerActionSheet.addAction(alertAction(title: "Choose Existing", withImagePickerController: .PhotoLibrary))
+        imagePickerActionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel) { (alert) -> Void in })
+        presentViewController(imagePickerActionSheet, animated: true, completion: nil)
     }
-
+    
+    // MARK: - Scale
+    
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        image.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+    
+    // MARK: - UIImagePickerController
+    
+    func showImagePickerControllerWithType(type: UIImagePickerControllerSourceType) {
+        let picker = imagePickerControllerWithType(type)
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerWithType(type: UIImagePickerControllerSourceType) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = type
+        return picker;
+    }
+    
 
 }
+
+extension ViewController: UIImagePickerControllerDelegate {
+    //
+}
+
 
